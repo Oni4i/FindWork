bf71f7eb3c4b8a8e6ee8c1e1f9dc3fe1015dc365
Nikita <websns@mail.ru>
2021-01-02 17:59:18 +0300

    delete trahs

--- resources/views/find/index.blade.php
+++ resources/views/find/index.blade.php
@@ -15,45 +15,6 @@
             </div>
         </div>
         <div class="row row-cols-1 row-cols-md-2 vacancies_row">
-            <div class="col mb-4">
-                <div class="card card-work">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                            additional content. This content is a little bit longer.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col mb-4">
-                <div class="card card-work">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting textt below as a natural lead-in to t
-                            below as a natural lead-in to t below as a natural lead-in to t below as a natural lead-in
-                            to below as a natural lead-in to additional content. This content is a little bit
-                            longer.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col mb-4">
-                <div class="card card-work">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                            additional content.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col mb-4">
-                <div class="card card-work">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
-                            additional content. This content is a little bit longer.</p>
-                    </div>
-                    <a href="fsd" class="btn btn-primary">Go to vacancy</a>
-                </div>
-            </div>
         </div>
     </div>
     <!-- Modal sites -->
